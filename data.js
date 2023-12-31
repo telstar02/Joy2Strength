7e0e6b5ffcb5b0744c564de6671fd98c283ff720
Telstar <Telstar@nwpu.edu>
2024-01-01 01:21:44 +0800

    24/1/1

---
+++ data.js
@@ -0,0 +1,50 @@
+const cardInfo = [
+    {
+        bgColor: '200, 6, 88',
+        imgUrl: './asset/img/8.jpg',
+        tips: 'HaoHe',
+        contnetText: '好喝。'
+    },
+    {
+        bgColor: '255,188,75',
+        imgUrl: './asset/img/7.jpg',
+        tips: '这是谁？',
+        contnetText: '现场很梦幻，因为我第一次去现场看/ᐠ｡ꞈ｡ᐟ\\'
+    },
+    {
+        bgColor: '152, 17, 249',
+        imgUrl: './asset/img/6.jpg',
+        tips: 'The Scenes',
+        contnetText: 'Ꮚ･ꈊ･Ꮚ'
+    },
+    {
+        bgColor: '255,188,75',
+        imgUrl: './asset/img/5.jpg',
+        tips: '天下没有不散的宴席',
+        contnetText: '下次再见了，朋友！'
+    },
+    {
+        bgColor: '243, 135, 7',
+        imgUrl: './asset/img/4.jpg',
+        tips: '发生什么了O.o',
+        contnetText: '应该有说不完的话哈哈哈哈哈哈哈>>>'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: './asset/img/3.jpg',
+        tips: '这好吗？',
+        contnetText: '虽然随意！但我很喜欢！谢谢！'
+    },
+    {
+        bgColor: '32,123,153',
+        imgUrl: './asset/img/2.jpg',
+        tips: '烟花',
+        contnetText: '好希望每年都能看到！'
+    },
+    {
+        bgColor: '157, 5, 248 ',
+        imgUrl: './asset/img/1.png',
+        tips: 'Q+A',
+        contnetText: 'HELLO:)'
+    },
+]
